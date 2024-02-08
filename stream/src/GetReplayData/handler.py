1533232f39bd88d3a74bf7dfdf69ec4734d92709
ThaminduSathruwan <sathruwan99@gmail.com>
2024-02-08 19:41:50 +0530

    BACKEND-11 complete getReplayData

--- stream/src/GetReplayData/handler.py
+++ stream/src/GetReplayData/handler.py
@@ -3,6 +3,7 @@ import pymysql
 from datetime import datetime
 import logging
 import os
+import base64
 
 
 # RDS settings
@@ -16,31 +17,29 @@ db_name = os.environ['RDS_DB_NAME']
 
 def handler(event, context):
     # Log the event argument for debugging and for use in local development.
-    print(json.dumps(event))
-
     return get_data_from_rds(event)
 
 
 def get_data_from_rds(event):
     try:
-        body = json.loads(event['body'])
-        start_timestamp = body['start_timestamp']
-        end_timestamp = body['end_timestamp']
-        
-        start_time = datetime.utcfromtimestamp(start_timestamp)
-        end_time = datetime.utcfromtimestamp(end_timestamp)
+        body = json.loads(base64.b64decode(event['body']))
+        print(body)
+        start_timestamp = body['start_time']
+        end_timestamp = body['end_time']
         
         # Connect to the database
         conn = pymysql.connect(host=rds_proxy_host, user=user_name, passwd=password, db=db_name, connect_timeout=5)
         
         cursor = conn.cursor()
         
-        query_get_transactions = "SELECT * FROM transactions t WHERE t.timestamp >= ? AND t.timestamp  <= ?"
-        cursor.execute(query_get_transactions, (start_time, end_time))
+        print("SUCCESS: Connection to RDS MySQL instance succeeded")
+        
+        query_get_transactions = "SELECT t.txn_hash, t.status, t.amount FROM transaction t WHERE t.insert_time >= %s AND t.insert_time  < %s"
+        cursor.execute(query_get_transactions, (start_timestamp, end_timestamp))
         result_transactions = cursor.fetchall()
         
-        query_get_blocks = "SELECT b.block_hash, bt.txn_hash FROM block b JOIN block_txn bt ON b.block_hash = bt.block_hash WHERE b.insert_time >= ? AND b.insert_time <= ?"
-        cursor.execute(query_get_blocks, (start_time, end_time))
+        query_get_blocks = "SELECT b.block_hash, b.previous_block_hash, GROUP_CONCAT(bt.txn_hash) FROM block b JOIN block_txn bt ON b.block_hash = bt.block_hash WHERE b.insert_time >= %s AND b.insert_time < %s GROUP BY b.block_hash"
+        cursor.execute(query_get_blocks, (start_timestamp, end_timestamp))
         result_blocks = cursor.fetchall()
         
         cursor.close()
@@ -52,13 +51,14 @@ def get_data_from_rds(event):
                 "Content-Type": "application/json"
             },
             "body": json.dumps({
-                "transactions": result_transactions,
-                "blocks": format_blocks(result_blocks)
+                "transactions": get_transaction_response(result_transactions),
+                "blocks": get_block_response(result_blocks)
             })
         }
-        
+                
         return response
     
+    # TODO: Add specific exceptions
     except Exception as e:
         logging.error(e)
         return {
@@ -67,17 +67,30 @@ def get_data_from_rds(event):
                 "Content-Type": "application/json"
             },
             "body": json.dumps({
-                # TODO: Have to handle the error messages
-                "error": "An error occurred" 
+                "error": "Internal Server Error"
             })
         }
-    
 
-def format_blocks(result_blocks):
-    block_dict = {}
-    for block_hash, txn_hash in result_blocks:
-        if block_hash not in block_dict:
-            block_dict[block_hash] = []
-        block_dict[block_hash].append(txn_hash)
+def get_transaction_response(result_transactions):
+    transaction_response = []
+    for txn in result_transactions:
+        txn_dict = {
+            "txn_hash": txn[0],
+            "status": txn[1],
+            "amount": txn[2]
+        }
+        transaction_response.append(txn_dict)
         
-    return block_dict
+    return transaction_response
+
+def get_block_response(result_blocks):
+    block_response = []
+    for block in result_blocks:
+        block_dict = {
+            "block_hash": block[0],
+            "previous_block_hash": block[1],
+            "txn_hashes": block[2]
+        }
+        block_response.append(block_dict)
+        
+    return block_response
