047663f0ee96f444da31b294b0e696fdb33348cb
ThaminduSathruwan <sathruwan99@gmail.com>
2024-02-08 21:41:50 +0530

    BACKEND-9 complete implementation

--- stream/src/GetRealTimeData/handler.py
+++ stream/src/GetRealTimeData/handler.py
@@ -3,9 +3,10 @@ import pymysql
 from datetime import datetime
 import logging
 import os
+import base64
 
 
-# rds settings 
+# RDS settings
 # TODO: Use AWS Secrets Manager to store credentials
 user_name = os.environ['RDS_USERNAME']
 password = os.environ['RDS_PASSWORD']
@@ -14,33 +15,34 @@ db_name = os.environ['RDS_DB_NAME']
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
-    
+
 
 def handler(event, context):
     # Log the event argument for debugging and for use in local development.
-    print(json.dumps(event))
-    return get_real_time_data(event)
+    return get_data_from_rds(event)
 
-def get_real_time_data(event):
+
+def get_data_from_rds(event):
     try:
-        body = json.loads(json.loads(event['body']))
-        last_timestamp = body['last_timestamp']
+        body = json.loads(base64.b64decode(event['body']))
+        start_timestamp = body['start_time']
+        end_timestamp = body['end_time']
         
-        date_time_obj = datetime.utcfromtimestamp(last_timestamp)
-        
-        conn = pymysql.connect(rds_proxy_host, user=user_name, passwd=password, db=db_name, connect_timeout=5)
-        print("SUCCESS: Connection to RDS mysql instance succeeded")
+        # Connect to the database
+        conn = pymysql.connect(host=rds_proxy_host, user=user_name, passwd=password, db=db_name, connect_timeout=5)
         
         cursor = conn.cursor()
         
-        query_get_transactions = "SELECT * FROM transaction WHERE insert_time > ?"
-        cursor.execute(query_get_transactions, (date_time_obj))
+        logger.info("SUCCESS: Able to connect to RDS MySQL instance")
+
+        query_get_transactions = "SELECT t.txn_hash, t.status, t.amount FROM transaction t WHERE t.insert_time >= %s AND t.insert_time  < %s"
+        cursor.execute(query_get_transactions, (start_timestamp, end_timestamp))
         result_transactions = cursor.fetchall()
         
-        query_get_blocks = "SELECT b.block_hash, bt.txn_hash FROM block b JOIN block_txn bt ON b.block_hash = bt.block_hash WHERE b.insert_time > ?" 
-        cursor.execute(query_get_blocks, (date_time_obj))
+        query_get_blocks = "SELECT b.block_hash, b.previous_block_hash, GROUP_CONCAT(bt.txn_hash) FROM block b JOIN block_txn bt ON b.block_hash = bt.block_hash WHERE b.insert_time >= %s AND b.insert_time < %s GROUP BY b.block_hash"
+        cursor.execute(query_get_blocks, (start_timestamp, end_timestamp))
         result_blocks = cursor.fetchall()
-                
+
         cursor.close()
         conn.close()
         
@@ -49,26 +51,60 @@ def get_real_time_data(event):
             "headers": {
                 "Content-Type": "application/json"
             },
-            "body": json.dumps({'transactions': result_transactions,
-                                'blocks': format_blocks(result_blocks)})
+            "body": json.dumps({
+                "transactions": get_transaction_response(result_transactions),
+                "blocks": get_block_response(result_blocks)
+            })
         }
-        
-        return response
-    
-    except Exception as e:
-        return {
+
+    except pymysql.MySQLError as e:
+        logger.error(f"ERROR: MySQL Error occurred: {e}")
+        response =  {
             "statusCode": 500,
             "headers": {
                 "Content-Type": "application/json"
             },
-            "body": json.dumps({"error": str(e)})
+            "body": json.dumps({
+                "error": "Internal Server Error"
+            })
         }
 
+    except Exception as e:
+        logger.error(f"ERROR: Internal Server Error: {e}")
+        response =  {
+            "statusCode": 500,
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "body": json.dumps({
+                "error": "Internal Server Error"
+            })
+        }
 
-def format_blocks(result_blocks):
-    blocks_dict = {}
-    for block_hash, txn_hash in result_blocks:
-        if block_hash not in blocks_dict:
-            blocks_dict[block_hash] = []
-        blocks_dict[block_hash].append(txn_hash)
-    return blocks_dict
+    return response
+
+
+def get_transaction_response(result_transactions):
+    transaction_response = []
+    for txn in result_transactions:
+        txn_dict = {
+            "txn_hash": txn[0],
+            "status": txn[1],
+            "amount": txn[2]
+        }
+        transaction_response.append(txn_dict)
+
+    return transaction_response
+
+
+def get_block_response(result_blocks):
+    block_response = []
+    for block in result_blocks:
+        block_dict = {
+            "block_hash": block[0],
+            "previous_block_hash": block[1],
+            "txn_hashes": block[2].split(",")
+        }
+        block_response.append(block_dict)
+
+    return block_response
