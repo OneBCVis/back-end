5c8228c3981a3c3dc0b24b365d6f0e145e8ccf84
Sasitha Kumarasinghe <sasithathathsara@outlook.com>
2024-02-23 20:06:15 +0530

    BACKEND-26 Unit test for `GetTransaction`

---
+++ stream/tests/unit/events/get_txn.py
@@ -0,0 +1,92 @@
+mock_event = {
+    "body": "{\"txn_hash\": \"0x97aaa6c1bc101abecd1ebb4e1a7c067a71b2c1e3f9ef304909af86e5e2f84c74\"}",
+    "headers": {
+        "Accept": "*/*",
+        "Accept-Encoding": "gzip, deflate",
+        "Connection": "keep-alive",
+        "Content-Length": "82",
+        "Host": "localhost:3000",
+        "User-Agent": "python-requests/2.31.0",
+        "X-Forwarded-Port": "3000",
+        "X-Forwarded-Proto": "http"
+    },
+    "httpMethod": "GET",
+    "isBase64Encoded": False,
+    "multiValueHeaders": {
+        "Accept": [
+            "*/*"
+        ],
+        "Accept-Encoding": [
+            "gzip, deflate"
+        ],
+        "Connection": [
+            "keep-alive"
+        ],
+        "Content-Length": [
+            "82"
+        ],
+        "Host": [
+            "localhost:3000"
+        ],
+        "User-Agent": [
+            "python-requests/2.31.0"
+        ],
+        "X-Forwarded-Port": [
+            "3000"
+        ],
+        "X-Forwarded-Proto": [
+            "http"
+        ]
+    },
+    "multiValueQueryStringParameters": None,
+    "path": "/transaction",
+    "pathParameters": None,
+    "queryStringParameters": None,
+    "requestContext": {
+        "accountId": "123456789012",
+        "apiId": "1234567890",
+        "domainName": "localhost:3000",
+        "extendedRequestId": None,
+        "httpMethod": "GET",
+        "identity": {
+            "accountId": None,
+            "apiKey": None,
+            "caller": None,
+            "cognitoAuthenticationProvider": None,
+            "cognitoAuthenticationType": None,
+            "cognitoIdentityPoolId": None,
+            "sourceIp": "127.0.0.1",
+            "user": None,
+            "userAgent": "Custom User Agent String",
+            "userArn": None
+        },
+        "path": "/transaction/",
+        "protocol": "HTTP/1.1",
+        "requestId": "fb33dd6d-b06b-495f-a03e-ff1e718553a9",
+        "requestTime": "23/Feb/2024:13:31:11 +0000",
+        "requestTimeEpoch": 1708695071,
+        "resourceId": "123456",
+        "resourcePath": "/transaction/",
+        "stage": "Prod"
+    },
+    "resource": "/transaction/",
+    "stageVariables": None
+}
+
+mock_txn = {
+    "txn_hash": "0x97aaa6c1bc101abecd1ebb4e1a7c067a71b2c1e3f9ef304909af86e5e2f84c74"
+}
+
+mock_txn_body = {
+    "txn_hash": "0x97aaa6c1bc101abecd1ebb4e1a7c067a71b2c1e3f9ef304909af86e5e2f84c74",
+    "status": "PENDING",
+    "amount": 1
+}
+
+
+def get_txn():
+    return {
+        "event": mock_event,
+        "txn": mock_txn,
+        "txn_body": mock_txn_body
+    }
