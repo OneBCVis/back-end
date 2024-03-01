a07c4d55be61d086b5aa49ac32cf676055901d5a
Sasitha Kumarasinghe <sasithathathsara@outlook.com>
2024-03-01 14:21:31 +0530

    BACKEND-31 Remove integer widths from database schema as integer width is deprecated

--- schema/ddl.sql
+++ schema/ddl.sql
@@ -2,9 +2,9 @@ create table if not exists block
 (
     block_hash          varchar(128)    not null,
     previous_block_hash varchar(128)    not null,
-    height              int(16)         not null,
-    nonce               int(16)         not null,
-    difficulty          int(16)         not null,
+    height              int             not null,
+    nonce               int             not null,
+    difficulty          int             not null,
     miner               varchar(128),
     time_stamp          varchar(32)     not null,
     insert_time         timestamp       not null default current_timestamp,
@@ -27,20 +27,27 @@ create table if not exists off_chain_data
     block_hash          varchar(128)    not null,
     id                  varchar(128)    not null,
     txn_id              varchar(128),
-    size                int(16)         not null,
+    size                int             not null,
     primary key (block_hash, id),
     foreign key (block_hash) references block(block_hash)
         on delete cascade
+);
+
+create table if not exists txn_type
+(
+    type       int         not null,
+    name       varchar(32) not null,
+    primary key (type)
 );
 
 create table if not exists transaction
 (
     txn_hash    varchar(128)                            not null,
     status      enum('PENDING', 'APPROVED', 'REJECTED') not null default 'PENDING',
-    amount      int(16)                                 not null,
-    type        int(4)                                  not null,
-    nonce       int(16)                                 not null,
-    fee         int(16)                                 not null,
+    amount      int                                     not null,
+    type        int                                     not null,
+    nonce       int                                     not null,
+    fee         int                                     not null,
     insert_time timestamp                               not null default current_timestamp,
     primary key (txn_hash),
     foreign key (type) references txn_type(type)
@@ -73,10 +80,4 @@ create table if not exists block_txn
         on delete cascade,
     foreign key (txn_hash) references transaction(txn_hash)
         on delete cascade
-);
-
-create table if not exists txn_type
-(
-    type       int(4)      not null,
-    name       varchar(32) not null
 );
